889007c328014dc4051ebe39ec057b40af2561fa
SKMNS\202110043 <jeonheejin@sk.com>
2023-06-05 09:20:20 +0900

    spring pointcut proxy

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -3,6 +3,8 @@ package hello.proxy;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -15,7 +17,9 @@ import org.springframework.context.annotation.Import;
 // @Import(InterfaceProxyConfig.class) // AppV2Config 을 스프링빈에 등록된다.
 // @Import(ConcreteProxyConfig.class) // AppV2Config 을 스프링빈에 등록된다.
 // @Import(DynamicProxyBasicConfig.class) // AppV2Config 을 스프링빈에 등록된다.
-@Import(DynamicProxyFilterConfig.class) // AppV2Config 을 스프링빈에 등록된다.
+//@Import(DynamicProxyFilterConfig.class) // AppV2Config 을 스프링빈에 등록된다.
+@Import(ProxyFactoryConfigV1.class) // AppV2Config 을 스프링빈에 등록된다.
+//@Import(ProxyFactoryConfigV2.class) // AppV2Config 을 스프링빈에 등록된다.
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // !주의 app 하위 에있는것만 스캔대상이 됨
 public class ProxyApplication {
 
